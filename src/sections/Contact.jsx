e23343e1d9a5b593ab4f021c6471b8df2a72ddc0
Shreyas-Patil-11 <shreyaspatil1311@gmail.com>
2025-01-26 22:17:08 +0530

    Fixing bugs

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,9 +1,12 @@
 import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react"
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
 
     const formRef = useRef();
+    const { alert, showAlert, hideAlert } = useAlert();
 
     const [loading, setLoading] = useState(false)
 
@@ -25,113 +28,126 @@ const Contact = () => {
         e.preventDefault();
         setLoading(true)
 
-        try {
-            await emailjs
-                .send(serviceId, templeteId, {
-                    from_name: form.name,
-                    to_name: "Shreyas",
-                    from_email: form.email,
-                    to_email: "shreyaspatil1311@gmail.com",
-                    message: form.message
-                },recieverId )
+        emailjs
+      .send(
+        serviceId,
+        templeteId,
+        {
+          from_name: form.name,
+          to_name: 'JavaScript Mastery',
+          from_email: form.email,
+          to_email: 'sujata@jsmastery.pro',
+          message: form.message,
+        },
+        recieverId,
+      )
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: 'Thank you for your message 😃',
+            type: 'success',
+          });
 
-            setLoading(false)
-            alert('Your message has been sent!')
-            setForm({ 
-                name: "", 
-                email: "", 
-                message: "" 
-            })
-        } catch (error){
-            setLoading(false)
-            console.log(error)
-            alert('Something went wrong')
-        }
+          setTimeout(() => {
+            hideAlert(false);
+            setForm({
+              name: '',
+              email: '',
+              message: '',
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: 'danger',
+          });
+        },
+      );
     };
 
     return (
-        <section className='c-space my-20' id="contact">
-            <div className='relative min-h-screen flex items-center justify-center flex-col'>
-                <img
-                    src="/assets/terminal.png"
-                    alt="terminal background"
-                    className='absolute insert-0 min-h-screen'
-                />
-                <div className='contact-container'>
-                    <h3 className='head-text'>Let&apos;s talk</h3>
-                    <p className="text-lg text-[#AFB0B6] mt-3">
-                        Whether you&apos;re looking to build a new website, improve your existing platform, or bring a unique project to
-                        life, I&apos;m here to help.
-                    </p>
+        <section className=" my-20" id="contact">
+      {alert.show && <Alert {...alert} />}
 
-                    <form
-                        ref={formRef}
-                        onSubmit={handleSubmit}
-                        className="mt-12 flex flex-col space-y-7"
-                    >
-                        <label className="space-y-3">
-                            <span className="field-label">
-                                Full Name
-                            </span>
-                            <input
-                                type="text"
-                                name="name"
-                                value={form.name}
-                                onChange={handleChange}
-                                required
-                                className="field-input"
-                                placeholder="John Doe"
-                            />
-                        </label>
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img 
+          src="/assets/terminal.png" 
+          alt="terminal-bg" 
+          className="absolute inset-0 min-h-screen contact_img" />
 
-                        <label className="space-y-3">
-                            <span className="field-label">
-                                E-mail
-                            </span>
-                            <input
-                                type="email"
-                                name="email"
-                                value={form.email}
-                                onChange={handleChange}
-                                required
-                                className="field-input"
-                                placeholder="johndoe@gmail.com"
-                            />
-                        </label>
+        <div className="contact-container">
+          <h3 className="head-text">Let&apos;s talk</h3>
+          <p className="text-lg text-[#AFB0B6] mt-3">
+            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
+            life, I’m here to help.
+          </p>
 
-                        <label className="space-y-3">
-                            <span className="field-label">
-                                Your message
-                            </span>
-                            <textarea
-                                name="message"
-                                value={form.message}
-                                onChange={handleChange}
-                                required
-                                rows={5}
-                                className="w-full bg-[#27272a] p-4 min-h-[3.5rem] rounded-lg text-lg text-[#d4d4d8] shadow-lg outline-none mt-4 resize-y placeholder-[#737373] "
-                                placeholder="Share your thoughts or inquiries..."
-                            />
-                        </label>
+          <form 
+            ref={formRef} 
+            onSubmit={handleSubmit} 
+            className="mt-12 flex flex-col space-y-7">
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., John Doe"
+              />
+            </label>
 
-                        <button
-                            className="field-btn"
-                            type="submit"
-                            disabled={loading}
-                        >
-                            {loading ? 'Sending...' : 'Send Message'}
-                            <img
-                                src="/assets/arrow-up.png"
-                                alt="arrow-up"
-                                className="w-2.5 h-2.5 object-contain invert brightness-0 "
-                            />
-                        </button>
+            <label className="space-y-3">
+              <span className="field-label">Email address</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., johndoe@gmail.com"
+              />
+            </label>
 
-                    </form>
-                </div>
-            </div>
-        </section>
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="w-full bg-[#27272a] p-4 min-h-[1rem] rounded-lg text-lg text-[#d4d4d8] shadow-lg outline-none mt-4 resize-y placeholder-[#737373]"
+                placeholder="Share your thoughts or inquiries..."
+              />
+            </label>
+
+            <button 
+              className="field-btn" 
+              type="submit" 
+              disabled={loading}>
+              {loading ? 'Sending...' : 'Send Message'}
+
+              <img 
+                src="/assets/arrow-up.png" 
+                alt="arrow-up" 
+                className="field-btn_arrow" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
     )
 }
 
-export default Contact
+export default Contact 
